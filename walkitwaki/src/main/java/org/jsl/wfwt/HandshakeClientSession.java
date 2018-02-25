ecff29652b9f47f8f927d766b911dbfec467aa8c
yuvraaz <yuvraaz@amniltech.com>
2018-02-25 14:20:34 +0545

    Final code

--- walkitwaki/src/main/java/org/jsl/wfwt/HandshakeClientSession.java
+++ walkitwaki/src/main/java/org/jsl/wfwt/HandshakeClientSession.java
@@ -1,6 +1,7 @@
 package org.jsl.wfwt;
 
 import android.util.Log;
+
 import org.jsl.collider.RetainableByteBuffer;
 import org.jsl.collider.Session;
 import org.jsl.collider.StreamDefragger;
@@ -10,8 +11,7 @@ import java.nio.ByteBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.util.concurrent.TimeUnit;
 
-public class HandshakeClientSession implements Session.Listener
-{
+public class HandshakeClientSession implements Session.Listener {
     private static final String LOG_TAG = HandshakeClientSession.class.getSimpleName();
 
     private final Channel m_channel;
@@ -23,17 +23,14 @@ public class HandshakeClientSession implements Session.Listener
     private final int m_pingInterval;
     private Runnable m_timerHandler;
 
-    private class TimerHandler implements Runnable
-    {
-        public void run()
-        {
-            Log.i( LOG_TAG, getLogPrefix() + "session timeout, close connection." );
+    private class TimerHandler implements Runnable {
+        public void run() {
+            Log.i(LOG_TAG, getLogPrefix() + "session timeout, close connection.");
             m_session.closeConnection();
         }
     }
 
-    private String getLogPrefix()
-    {
+    private String getLogPrefix() {
         return m_channel.getName() + " (" + m_serviceName + ", " + m_session.getRemoteAddress() + "): ";
     }
 
@@ -45,8 +42,7 @@ public class HandshakeClientSession implements Session.Listener
             Session session,
             SessionManager sessionManager,
             TimerQueue timerQueue,
-            int pingInterval )
-    {
+            int pingInterval) {
         m_channel = channel;
         m_serviceName = serviceName;
         m_session = session;
@@ -56,131 +52,97 @@ public class HandshakeClientSession implements Session.Listener
         m_pingInterval = pingInterval;
 
         /* Timer should be started before handshake request send. */
-        if (pingInterval > 0)
-        {
+        if (pingInterval > 0) {
             m_timerHandler = new TimerHandler();
-            timerQueue.schedule( m_timerHandler, pingInterval, TimeUnit.SECONDS );
+            timerQueue.schedule(m_timerHandler, pingInterval, TimeUnit.SECONDS);
         }
 
-        try
-        {
-            final ByteBuffer handshakeRequest = Protocol.HandshakeRequest.create( audioFormat, stationName );
-            session.sendData( handshakeRequest );
-        }
-        catch (final CharacterCodingException ex)
-        {
-            Log.e( LOG_TAG, getLogPrefix() + ex.toString() );
+        try {
+            final ByteBuffer handshakeRequest = Protocol.HandshakeRequest.create(audioFormat, stationName);
+            session.sendData(handshakeRequest);
+        } catch (final CharacterCodingException ex) {
+            Log.e(LOG_TAG, getLogPrefix() + ex.toString());
             session.closeConnection();
         }
     }
 
-    public void onDataReceived( RetainableByteBuffer data )
-    {
-        if (m_timerHandler != null)
-        {
-            try
-            {
-                final int rc = m_timerQueue.cancel( m_timerHandler );
-                if (rc != 0)
-                {
+    public void onDataReceived(RetainableByteBuffer data) {
+        if (m_timerHandler != null) {
+            try {
+                final int rc = m_timerQueue.cancel(m_timerHandler);
+                if (rc != 0) {
                     /* Timer already fired, Session.closeConnection is called,
                      * or will be called very soon, so no any sense to handle anything.
                      */
                     return;
                 }
-            }
-            catch (final InterruptedException ex)
-            {
-                Log.w( LOG_TAG, ex.toString() );
+            } catch (final InterruptedException ex) {
+                Log.w(LOG_TAG, ex.toString());
                 Thread.currentThread().interrupt();
             }
         }
 
-        final RetainableByteBuffer msg = m_streamDefragger.getNext( data );
-        if (msg == null)
-        {
+        final RetainableByteBuffer msg = m_streamDefragger.getNext(data);
+        if (msg == null) {
             /* HandshakeReply is fragmented, strange but can happen */
-            Log.i( LOG_TAG, getLogPrefix() + "fragmented <HandshakeReply>." );
-        }
-        else if (msg == StreamDefragger.INVALID_HEADER)
-        {
-            Log.i( LOG_TAG, getLogPrefix() +
-                    "invalid <HandshakeReply> received, close connection." );
+            Log.i(LOG_TAG, getLogPrefix() + "fragmented <HandshakeReply>.");
+        } else if (msg == StreamDefragger.INVALID_HEADER) {
+            Log.i(LOG_TAG, getLogPrefix() +
+                    "invalid <HandshakeReply> received, close connection.");
             m_session.closeConnection();
-        }
-        else
-        {
-            final int messageID = Protocol.Message.getID( msg );
-            if (messageID == Protocol.HandshakeReplyOk.ID)
-            {
-                try
-                {
-                    final String audioFormat = Protocol.HandshakeReplyOk.getAudioFormat( msg );
-                    final String stationName = Protocol.HandshakeReplyOk.getStationName( msg );
+        } else {
+            final int messageID = Protocol.Message.getID(msg);
+            if (messageID == Protocol.HandshakeReplyOk.ID) {
+                try {
+                    final String audioFormat = Protocol.HandshakeReplyOk.getAudioFormat(msg);
+                    final String stationName = Protocol.HandshakeReplyOk.getStationName(msg);
                     final AudioPlayer audioPlayer = AudioPlayer.create(
-                            getLogPrefix(), audioFormat, m_channel, m_serviceName, m_session );
-                    if (audioPlayer == null)
-                    {
-                        Log.w( LOG_TAG, getLogPrefix() +
-                                "unsupported audio format [" + audioFormat + "], closing connection" );
+                            getLogPrefix(), audioFormat, m_channel, m_serviceName, m_session);
+                    if (audioPlayer == null) {
+                        Log.w(LOG_TAG, getLogPrefix() +
+                                "unsupported audio format [" + audioFormat + "], closing connection");
                         m_session.closeConnection();
-                    }
-                    else
-                    {
-                        Log.i( LOG_TAG, getLogPrefix() +
-                                "HandshakeReplyOk: audioFormat[" + audioFormat + "] stationName[" + stationName + "]" );
+                    } else {
+                        Log.i(LOG_TAG, getLogPrefix() +
+                                "HandshakeReplyOk: audioFormat[" + audioFormat + "] stationName[" + stationName + "]");
 
                         final ChannelSession channelSession = new ChannelSession(
                                 m_channel, m_serviceName, m_session, m_streamDefragger, m_sessionManager, audioPlayer, m_timerQueue, m_pingInterval);
 
-                        m_channel.setStationInfo( m_serviceName, channelSession, stationName );
-                        m_session.replaceListener( channelSession );
+                        m_channel.setStationInfo(m_serviceName, channelSession, stationName);
+                        m_session.replaceListener(channelSession);
                     }
-                }
-                catch (final CharacterCodingException ex)
-                {
-                    Log.w( LOG_TAG, getLogPrefix() + ": " + ex.toString() + ", close connection" );
+                } catch (final CharacterCodingException ex) {
+                    Log.w(LOG_TAG, getLogPrefix() + ": " + ex.toString() + ", close connection");
                     m_session.closeConnection();
                 }
-            }
-            else if (messageID == Protocol.HandshakeReplyFail.ID)
-            {
+            } else if (messageID == Protocol.HandshakeReplyFail.ID) {
                 String statusText = null;
-                try
-                {
-                    statusText = Protocol.HandshakeReplyFail.getStatusText( msg );
+                try {
+                    statusText = Protocol.HandshakeReplyFail.getStatusText(msg);
+                } catch (final CharacterCodingException ex) {
+                    Log.w(LOG_TAG, getLogPrefix() + ex.toString());
                 }
-                catch (final CharacterCodingException ex)
-                {
-                    Log.w( LOG_TAG, getLogPrefix() + ex.toString() );
-                }
-                Log.i( LOG_TAG, getLogPrefix() + "HandshakeRequest rejected: " + statusText + ", close connection." );
+                Log.i(LOG_TAG, getLogPrefix() + "HandshakeRequest rejected: " + statusText + ", close connection.");
                 m_session.closeConnection();
-            }
-            else
-            {
-                Log.i( LOG_TAG, getLogPrefix() +
+            } else {
+                Log.i(LOG_TAG, getLogPrefix() +
                         "unexpected message " + messageID + " received from " +
-                        m_session.getRemoteAddress() + ", close connection." );
+                        m_session.getRemoteAddress() + ", close connection.");
                 m_session.closeConnection();
             }
         }
     }
 
-    public void onConnectionClosed()
-    {
-        Log.i( LOG_TAG, getLogPrefix() + ": connection closed" );
+    public void onConnectionClosed() {
+        Log.i(LOG_TAG, getLogPrefix() + ": connection closed");
         boolean interrupted = false;
 
-        if (m_timerHandler != null)
-        {
-            try
-            {
-                m_timerQueue.cancel( m_timerHandler );
-            }
-            catch (final InterruptedException ex)
-            {
-                Log.w( LOG_TAG, ex.toString(), ex );
+        if (m_timerHandler != null) {
+            try {
+                m_timerQueue.cancel(m_timerHandler);
+            } catch (final InterruptedException ex) {
+                Log.w(LOG_TAG, ex.toString(), ex);
                 interrupted = true;
             }
         }
